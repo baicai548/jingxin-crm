3264194a97f9a6bd1719adc3a890297733aabe3c
baicai548 <165147980+baicai548@users.noreply.github.com>
2026-05-14 16:42:24 +0800

    Update render_start.sh

--- render_start.sh
+++ render_start.sh
@@ -3,8 +3,7 @@ python -c "
 from app import app
 from models import db
 with app.app_context():
-    db.drop_all()
     db.create_all()
-    print('DB Reset OK')
+    print('DB OK')
 "
 gunicorn app:app --bind 0.0.0.0:$PORT
