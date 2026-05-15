c9b6b5e028f2d92aec6343ed2fee0eb9e69abeb1
baicai548 <165147980+baicai548@users.noreply.github.com>
2026-05-15 17:25:56 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
 import os
+
 class Config:
     SECRET_KEY = 'jingxin-crm-secret-key-2026'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///crm.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # 百度OCR配置
+    BAIDU_OCR_API_KEY = 'XABbET6H7QZWCMUsAiaemG0t'
+    BAIDU_OCR_SECRET_KEY = '8b3k65e69Q13VWbPJAlyL047B1Zs9FN2'
