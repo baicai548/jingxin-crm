147fd0dfc406869599dacb282309cde21d75a201
baicai548 <165147980+baicai548@users.noreply.github.com>
2026-05-15 08:52:05 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 class Config:
     SECRET_KEY = 'jingxin-crm-secret-key-2026'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///crm.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///crm.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
