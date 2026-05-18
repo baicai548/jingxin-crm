1a0d79bc80bca3cb0f245c216966132465b4fd9b
baicai548 <165147980+baicai548@users.noreply.github.com>
2026-05-18 17:34:40 +0800

    Update render_start.sh

--- render_start.sh
+++ render_start.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
-python -c "from app import app, db; app.app_context().push(); db.create_all(); print('Tables created!')"
+python -c "
+from app import app, db
+app.app_context().push()
+db.drop_all()
+db.create_all()
+print('Tables recreated!')
+"
 gunicorn app:app --bind 0.0.0.0:$PORT
