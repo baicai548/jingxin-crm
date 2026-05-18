a369ae9f7e3e3475f360212da0507a37e4a1e158
baicai548 <165147980+baicai548@users.noreply.github.com>
2026-05-18 17:43:01 +0800

    Update render_start.sh

--- render_start.sh
+++ render_start.sh
@@ -1,9 +1,16 @@
+
 #!/usr/bin/env bash
 python -c "
+import os, sys
+sys.path.insert(0, '.')
+os.environ.setdefault('DATABASE_URL', '')
+database_url = os.environ.get('DATABASE_URL', '')
+if database_url.startswith('postgres://'):
+    os.environ['DATABASE_URL'] = database_url.replace('postgres://', 'postgresql://', 1)
 from app import app, db
-app.app_context().push()
-db.drop_all()
-db.create_all()
-print('Tables recreated!')
+with app.app_context():
+    db.drop_all()
+    db.create_all()
+    print('Tables recreated!')
 "
 gunicorn app:app --bind 0.0.0.0:$PORT
